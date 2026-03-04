50e245ff7f39a27fa1ca3434a6d50a83e94c6282
kawashita kenji <leonardkenji@gmail.com>
2026-03-04 10:57:41 +0900

    ui components

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @item = Item.find(params[:item_id])
+  end
+private
+  def item_params
+    params.require(:item).permit(:name, :category, :description, :slot)
+  end
 end
