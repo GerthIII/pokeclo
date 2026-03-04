bed06b3165cc161bdd2ae25f14abeaf470228c8a
Kohsei Shioda <tako.sachezsan@gmail.com>
2026-03-04 15:37:44 +0900

    index and edit were edited

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -2,7 +2,7 @@ class Outfit < ApplicationRecord
   has_many :outfit_items
   has_many :items, through: :outfit_items
   has_many :messages
-  has_one_attached :image
+  has_one_attached :jacket
   belongs_to :user
 
   validates :name, presence: true
