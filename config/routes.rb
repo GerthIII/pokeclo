7ba8ccf44ac346dd563fb9391c6e50233c48eb61
Ryota Hayakawa <hykwryota98913@gmail.com>
2026-03-06 12:47:01 +0900

    Added some codes to PWA and icon images

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,6 @@ end
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
