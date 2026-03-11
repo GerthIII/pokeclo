3377907d4560b4b422a3c9e5570131309ecc5cf5
Glen Gerth <glengerth3@gmail.com>
2026-03-11 10:51:47 +0900

    Changed the redirect from items index to item show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
     authorize @item
 
     if @item.save
-      redirect_to items_path(@item)
+      redirect_to item_path(@item)
     else
       render :new, status: :unprocessable_entity
     end
